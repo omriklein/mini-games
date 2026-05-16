a73c84a78676616c81d671230a3b7d1e0df4b50d
omriklein <omrik2007@gmail.com>
2026-05-16 08:40:12 +0300

    Rewrite snake in golf style

--- snake/snake.js
+++ snake/snake.js
@@ -1,67 +1,31 @@
 
+R=a=>Math.random()*a|0
+w=20,h=20,W=w+2,d=1,G=0
+s=[1+(w>>1)+((h>>1)+1)*W]
+S = new Set(s)
+for(i=0;i<W*(h+2);i++)(i/W<1||i/W>h||i%W<1||i%W>w)&&S.add(i)
 
-const width = 20, height = 20;
-const cellSize = 20;
+(F=() => {
+    while(S.has(f = R(w)+1 + (R(h)+1)*W));
+})()
 
-const canvas = document.createElement("canvas");
-canvas.width = width*cellSize;
-canvas.height = height*cellSize;
-const ctx = canvas.getContext("2d");
-document.body.appendChild(canvas);
-const snake = [[0/2, height/2]] // x y
-dir = [1, 0] // x y
-gameOver = false
+(D=()=>{
+    t = "<table>"
+    for(i = W+1; i < W*(h+1); i++)
+        t += (k=i%W)==0 ? '<tr>' : k==W-1 ? '' : `<th width=20 height=20 bgcolor='${S.has(i)?'blue':i==f?'red':'green'}'>`
+    document.body.innerHTML = t + (G ? "GAME OVER" : "")
+})()
 
-const random = (a) => Math.floor(Math.random() * (a + 1));
-foodx = random(width), foody = random(height)
+I=setInterval(() => {
+    G && clearInterval(I)
+    n = s[0] + d
+    if(S.has(n)) { G = 1; D(); return }
+    s.unshift(n), S.add(n)
+    if(n != f) S.delete(s.pop())
+    else F()
+    D()
+},300)
 
-const walk=()=>{
-    const next = [snake[0][0]+dir[0],snake[0][1]+dir[1]]
-    snake.unshift(next);
-    if(next[0] != foodx || next[1] != foody) snake.pop()
-    else spawnFood()
-    draw();
-}
-
-const draw=()=>{
-    ctx.fillStyle = "green";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    console.log(snake.length)
-    ctx.fillStyle = "blue";
-    for([l,k] of snake){
-        ctx.fillRect(l*cellSize, k*cellSize, cellSize, cellSize);
-    }
-
-    ctx.fillStyle = "red"
-    ctx.fillRect(foodx*cellSize, foody*cellSize, cellSize, cellSize);
-}
-draw();
-
-const spawnFood = () => {
-    foodx = random(width), foody = random(height)
-    while(snake.some(a => a[0] == foodx && a[1] == foody)) { 
-        foodx = random(width), foody = random(height)
-    }
-}
-
-window.addEventListener("keydown", (event) => {
-  switch (event.key) {
-    case "ArrowUp":
-      dir=[0,-1];
-      break;
-    case "ArrowDown":
-      dir=[0,1];
-      break;
-    case "ArrowLeft":
-      dir=[-1,0];
-      break;
-    case "ArrowRight":
-      dir=[1,0];
-      break;
-  }
-});
-
-
-// setInterval(() => !gameOver && walk(), 300)
-spawnFood()
+window.addEventListener("keydown", e => {
+    d={"ArrowUp": -W, "ArrowDown": W, "ArrowLeft": -1, "ArrowRight": 1}[e.key]??d
+})
